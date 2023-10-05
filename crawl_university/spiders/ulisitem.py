163f79d381becd6f800e7a18636a6dbf81809937
DucTrung611 <thanductrung123@gmail.com>
2023-10-05 13:03:51 +0700

    rename file

--- crawl_university/spiders/ulisitem.py
+++ crawl_university/spiders/ulisitem.py
@@ -1,7 +1,6 @@
 import scrapy
 import json
 
-
 class UlisitemSpider(scrapy.Spider):
     name = "ulisitem"
     start_urls = []
@@ -13,14 +12,15 @@ class UlisitemSpider(scrapy.Spider):
             self.start_urls = [item['link'] for item in data]
 
     def parse(self, response):
+        # Sử dụng CSS selector để lấy tiêu đề và nội dung
         titles = response.css('h2.title::text').getall()
-        content = response.css('p::text').getall()
+        content = response.css('div.single-content::text').getall()
         
-
         articles = []
 
+        # Lặp qua các tiêu đề và nội dung để tạo danh sách bài báo
         for title, paragraph in zip(titles, content):
-            article = {"title": title, "content": paragraph}
+            article = {"title": title.strip(), "content": paragraph.strip()}
             articles.append(article)
 
         # Trả về từng bài báo dưới dạng danh sách động
